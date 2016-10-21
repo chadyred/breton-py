d202369aa4addab5b6b0a0737d8f2f5ff7d17471
CELLIER Florian <cellier.florian@hotmail.fr>
2016-10-21 17:35:48 +0200

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/www-dev/private/perso/django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ crepes_bretonnes/crepes_bretonnes/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from blog import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'crepes_bretonnes.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accueil$', views.home),
+]
